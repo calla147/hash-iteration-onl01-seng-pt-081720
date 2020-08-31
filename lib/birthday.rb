0c8ba88f9ec85a72ca17f102f71b2d2fce4b0b79
noella callahan <calla147@gmail.com>
2020-08-31 01:18:24 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -4,10 +4,19 @@
 #   "Amanda" => 27
 # }
 
-def happy_birthday(birthday_kids)
-  birthday_kids.each do |name, age|
-    puts "Happy Birthday #{name}! You are now #{age} years old!"
+def  birthday_kids.each do |kids_name, age|
+    puts ("Happy Birthday #{kids_name}! You are now #{age} years old!")
   end
+end 
+
+def age_appropriate_birthday(birthday_kids)
+ younger_birthday = {}
+  birthday_kids.each do |kids_name, age|
+    if age <= 12
+      younger_birthday[kids_name] = age
+      puts ("Happy Birthday #{kids_name}! You are now #{age} years old!")
+    else
+      puts "You are too old for this."
+    end
+  end 
 end
-  def  age_appropriate_birthday
- end
